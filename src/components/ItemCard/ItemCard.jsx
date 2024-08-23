3fcdfde7eadf9e163e098cd4ff50a1ddd569c1ce
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-08-24 01:41:57 +0530

    Added product

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,22 +1,26 @@
 import styles from "./ItemCard.module.css";
 import Image from "next/image";
 
-const ItemCard = ({ artistHandle, itemDetails }) => {
-    return (
-        <div className={styles.card}>
-            <Image src={`/artist/${artistHandle}/merch/${itemDetails.itemCode}.png`} width="200" height="200" alt="" className={styles.cardImage}/>
-            <div className={styles.cardDescription}>
-                <h5> {itemDetails.name} </h5>
-                <div className={styles.price}>
-                    Rs.{itemDetails.price}
-                </div>
+const ItemCard = ({artistHandle, itemDetails}) => {
+    return (<div className={styles.card}>
+        <Image src={`/artist/${artistHandle}/merch/${itemDetails.itemCode}.png`} width="200" height="200" alt=""
+               className={styles.cardImage}/>
+        <div className={styles.cardDescription}>
+            <h5> {itemDetails.name} </h5>
+            <div className={styles.price}>
+                Rs.{itemDetails.price}
+            </div>
 
+            <div className={styles.chipRow}>
                 <span className={styles.chip}>
-                {itemDetails.quantity > 0 ? "Available" : "Out of Stock"}
-            </span>
+                    {itemDetails.quantity > 0 ? "Available" : "Out of Stock"}
+                </span>
+                <span className={styles.chip}>
+                    {itemDetails.quantity}
+                </span>
             </div>
         </div>
-    )
+    </div>)
 }
 
 export default ItemCard;
